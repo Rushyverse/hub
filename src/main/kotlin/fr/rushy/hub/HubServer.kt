f07c14ef82009573a8671f58098c434024d5b1bc
Distractic <distractic@outlook.fr>
2022-12-19 18:51:12 +0100

    fix: Use configuration file from arg

--- src/main/kotlin/fr/rushy/hub/HubServer.kt
+++ src/main/kotlin/fr/rushy/hub/HubServer.kt
@@ -1,18 +1,14 @@
 package fr.rushy.hub
 
 import fr.rushy.api.RushyServer
-import fr.rushy.api.configuration.Configuration
 import fr.rushy.hub.configuration.HubConfiguration
 import fr.rushy.hub.listener.PlayerLoginListener
 import fr.rushy.hub.listener.PlayerMoveListener
 import fr.rushy.hub.listener.PlayerSpawnListener
 import fr.rushy.hub.listener.PlayerStartFlyingListener
-import mu.KotlinLogging
 import net.minestom.server.MinecraftServer
 import net.minestom.server.event.GlobalEventHandler
 import net.minestom.server.instance.InstanceContainer
-
-private val logger = KotlinLogging.logger { }
 
 class HubServer {
 
@@ -22,7 +18,7 @@ class HubServer {
 
         @JvmStatic
         override fun main(args: Array<String>) {
-            start<HubConfiguration> {
+            start<HubConfiguration>(args.firstOrNull()) {
                 val translationsProvider = createTranslationsProvider(listOf(BUNDLE_HUB))
 
                 registerCommands()
