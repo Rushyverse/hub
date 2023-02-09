c6d06b5a57ead659fd4c1e6dfd2e7e6b2192fe7e
Quentin <nelsonsims@hotmail.fr>
2023-02-09 14:06:32 +0100

    chore(database): Implements friend database service

--- src/main/kotlin/com/github/rushyverse/hub/HubServer.kt
+++ src/main/kotlin/com/github/rushyverse/hub/HubServer.kt
@@ -2,8 +2,14 @@ package com.github.rushyverse.hub
 
 import com.github.rushyverse.api.RushyServer
 import com.github.rushyverse.api.translation.TranslationsProvider
+import com.github.rushyverse.core.cache.CacheClient
+import com.github.rushyverse.core.data.FriendCacheService
+import com.github.rushyverse.core.data.FriendService
+import com.github.rushyverse.core.supplier.database.DatabaseSupplierServices
+import com.github.rushyverse.core.supplier.database.IDatabaseEntitySupplier
 import com.github.rushyverse.hub.command.EmoteCommand
 import com.github.rushyverse.hub.configuration.HubConfiguration
+import com.github.rushyverse.hub.database.friends.FriendServiceDatabase
 import com.github.rushyverse.hub.items.hotbar.HotbarItemsManager
 import com.github.rushyverse.hub.listener.PlayerLoginListener
 import com.github.rushyverse.hub.listener.PlayerMoveListener
@@ -15,6 +21,14 @@ import com.github.rushyverse.hub.listener.item.PlayerDropItemListener
 import com.github.rushyverse.hub.listener.item.PlayerInventoryClickListener
 import com.github.rushyverse.hub.listener.item.PlayerItemClickListener
 import com.github.rushyverse.hub.listener.item.PlayerSwapItemListener
+import io.github.universeproject.kotlinmojangapi.MojangAPI
+import io.github.universeproject.kotlinmojangapi.MojangAPIImpl
+import io.ktor.client.*
+import io.ktor.client.engine.cio.*
+import io.ktor.client.plugins.contentnegotiation.*
+import io.ktor.serialization.kotlinx.json.*
+import io.lettuce.core.RedisURI
+import kotlinx.serialization.json.Json
 import net.minestom.server.MinecraftServer
 import net.minestom.server.event.GlobalEventHandler
 import net.minestom.server.instance.InstanceContainer
@@ -29,6 +43,9 @@ class HubServer(private val configuration: String? = null) : RushyServer() {
         const val BUNDLE_HUB = "hub"
     }
 
+    lateinit var friendService: FriendService private set
+    lateinit var mojangAPI: MojangAPI private set
+
     override suspend fun start() {
         start<HubConfiguration>(configuration) {
             val translationsProvider = createTranslationsProvider(
@@ -38,6 +55,16 @@ class HubServer(private val configuration: String? = null) : RushyServer() {
                 )
             )
 
+            // Database
+            val cacheClient = createCacheClient()
+            val services = DatabaseSupplierServices(
+                FriendCacheService(cacheClient) to FriendServiceDatabase(),
+            )
+            friendService = FriendService(IDatabaseEntitySupplier.cacheWithCachingDatabaseFallback(services))
+            // cacheWithCachingDatabaseFallback(services)); - Gives the wanted results but in x2 duplication
+
+            mojangAPI = MojangAPIImpl(createHttpClient())
+
             API.registerCommands()
 
             val globalEventHandler = MinecraftServer.getGlobalEventHandler()
@@ -46,6 +73,23 @@ class HubServer(private val configuration: String? = null) : RushyServer() {
             addCommands()
 
             MinecraftServer.setBrandName("Rushyverse")
+
+
+        }
+    }
+
+    suspend fun createCacheClient(): CacheClient {
+        return CacheClient {
+            uri = RedisURI.create("redis://default:redispw@localhost:49153")
+        }
+    }
+
+    fun createHttpClient(): HttpClient {
+        return HttpClient(CIO) {
+            expectSuccess = true
+            install(ContentNegotiation) {
+                json(Json { ignoreUnknownKeys = true })
+            }
         }
     }
 
@@ -60,7 +104,12 @@ class HubServer(private val configuration: String? = null) : RushyServer() {
     ) {
         globalEventHandler.addListener(PlayerStartFlyingListener())
         globalEventHandler.addListener(PlayerLoginListener(instanceContainer))
-        globalEventHandler.addListener(PlayerSpawnListener(translationsProvider, HotbarItemsManager(translationsProvider)))
+        globalEventHandler.addListener(
+            PlayerSpawnListener(
+                translationsProvider,
+                HotbarItemsManager(friendService, mojangAPI, translationsProvider)
+            )
+        )
         globalEventHandler.addListener(PlayerMoveListener())
         globalEventHandler.addListener(PlayerItemClickListener())
         globalEventHandler.addListener(PlayerDropItemListener())
