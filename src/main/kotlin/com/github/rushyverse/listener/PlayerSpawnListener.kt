3b18582b1e95a1feb92aaa03932cac480e3e4d88
Quentin <nelsonsims@hotmail.fr>
2023-03-19 16:35:30 +0100

    chore: Remove commented code

--- src/main/kotlin/com/github/rushyverse/listener/PlayerSpawnListener.kt
+++ src/main/kotlin/com/github/rushyverse/listener/PlayerSpawnListener.kt
@@ -5,13 +5,9 @@ import com.github.rushyverse.api.extension.sync
 import com.github.rushyverse.api.translation.SupportedLanguage
 import com.github.rushyverse.api.translation.TranslationsProvider
 import com.github.rushyverse.configuration.ScoreboardConfiguration
-import net.kyori.adventure.text.Component
-import net.kyori.adventure.text.format.NamedTextColor
 import net.minestom.server.coordinate.Pos
-import net.minestom.server.entity.Player
 import net.minestom.server.event.EventListener
 import net.minestom.server.event.player.PlayerSpawnEvent
-import net.minestom.server.scoreboard.Sidebar
 
 class PlayerSpawnListener(
     private val translationsProvider: TranslationsProvider,
@@ -36,18 +32,6 @@ class PlayerSpawnListener(
             )
             .addViewer(this)
         }
-
-       /* event.player.getAcquirable<Player>().sync { player ->
-            val sidebar = Sidebar(Component.text("Sidebar"))
-            sidebar.createLine(
-                Sidebar.ScoreboardLine(
-                    "score",
-                    Component.text("test", NamedTextColor.RED),
-                    0
-                )
-            )
-            sidebar.addViewer(player)
-        }*/
 
         return EventListener.Result.SUCCESS
     }
