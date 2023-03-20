0c2d0cc96a0829a013e6f38a389404d5604f6970
Quentin <nelsonsims@hotmail.fr>
2023-03-20 11:17:38 +0100

    chore: Create ext. function setLocale for Player

--- src/main/kotlin/com/github/rushyverse/HubPlayer.kt
+++ src/main/kotlin/com/github/rushyverse/HubPlayer.kt
@@ -5,22 +5,16 @@ import net.minestom.server.entity.Player
 import net.minestom.server.network.player.PlayerConnection
 import java.util.*
 
+fun Player.setLocale(language: SupportedLanguage) {
+    this.locale = language.locale
+}
+
 class HubPlayer(
     uuid: UUID,
     username: String,
     playerConnection: PlayerConnection,
-    var language: SupportedLanguage = SupportedLanguage.ENGLISH,
     var prestige: Int = 0,
     var tokens: Int = 0,
     var experience: Double = 0.0,
     var friends: Set<UUID> = emptySet()
-) : Player(uuid, username, playerConnection) {
-
-    init {
-        locale = language.locale
-    }
-
-    override fun getLocale(): Locale {
-        return language.locale
-    }
-}
+) : Player(uuid, username, playerConnection)
