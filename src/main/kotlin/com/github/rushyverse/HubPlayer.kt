e68b1022a2ec67795cea601278b9fa0fd92eb59f
Quentin <nelsonsims@hotmail.fr>
2023-03-20 08:53:30 +0100

    chore: Create HubPlayer

---
+++ src/main/kotlin/com/github/rushyverse/HubPlayer.kt
@@ -0,0 +1,22 @@
+package com.github.rushyverse
+
+import com.github.rushyverse.api.translation.SupportedLanguage
+import net.minestom.server.entity.Player
+import net.minestom.server.network.player.PlayerConnection
+import java.util.*
+
+class HubPlayer(
+    uuid: UUID,
+    username: String,
+    playerConnection: PlayerConnection,
+    language: SupportedLanguage = SupportedLanguage.ENGLISH,
+    var prestige: Int = 0,
+    var tokens: Int = 0,
+    var experience: Double = 0.0,
+    var friends:Set<UUID> = emptySet()
+) : Player(uuid, username, playerConnection) {
+
+    init {
+        locale = language.locale
+    }
+}
