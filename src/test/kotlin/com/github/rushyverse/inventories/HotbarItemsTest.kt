c622aa41495345cceafe48cfe1bbcf27af3c2f00
Quentin <nelsonsims@hotmail.fr>
2023-03-29 22:21:49 +0200

    chore: Initializing tests

---
+++ src/test/kotlin/com/github/rushyverse/inventories/HotbarItemsTest.kt
@@ -0,0 +1,77 @@
+package com.github.rushyverse.inventories
+
+import com.github.rushyverse.AbstractTest
+import kotlinx.coroutines.test.runTest
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Nested
+
+class HotbarItemsTest : AbstractTest() {
+
+    @Nested
+    inner class CreateItems {
+        @Test
+        fun `should create the cosmetics item`()= runTest {
+            // TODO
+        }
+
+        @Test
+        fun `should create the main menu item`()= runTest {
+            // TODO
+        }
+
+        @Test
+        fun `should create the stats item`()= runTest {
+            // TODO
+        }
+
+        @Test
+        fun `should create the settings item`()= runTest {
+            // TODO
+        }
+
+        @Test
+        fun `should create the lang item`()= runTest {
+            // TODO
+        }
+
+        @Test
+        fun `should create the social item`()= runTest {
+            // TODO
+        }
+    }
+
+    @Nested
+    inner class PlaceItems {
+        @Test
+        fun `should place cosmetics item on slot 3`() = runTest {
+            // TODO
+        }
+
+        @Test
+        fun `should place main menu item on slot 4`() = runTest {
+            // TODO
+        }
+
+        @Test
+        fun `should place stats item on slot 5`() = runTest {
+            // TODO
+        }
+
+        @Test
+        fun `should place settings item on slot 20`() = runTest {
+            // TODO
+        }
+
+        @Test
+        fun `should place lang item on slot 22`() = runTest {
+            // TODO
+        }
+
+        @Test
+        fun `should place social item on slot 24`() = runTest {
+            // TODO
+        }
+    }
+
+}
