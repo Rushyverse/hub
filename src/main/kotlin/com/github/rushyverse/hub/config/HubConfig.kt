1fe56c80889edc193f4968bded0df4fc8cf4c266
Cizetux <cizetux@gmail.com>
2023-08-04 18:01:46 +0200

    chore: Update config data classes

--- src/main/kotlin/com/github/rushyverse/hub/config/HubConfig.kt
+++ src/main/kotlin/com/github/rushyverse/hub/config/HubConfig.kt
@@ -1,25 +1,16 @@
 package com.github.rushyverse.hub.config
 
 import com.github.rushyverse.hub.config.game.GamesGUIConfig
-import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 import org.bukkit.Material
 
 @Suppress("PROVIDED_RUNTIME_TOO_LOW")
 @Serializable
 data class HubConfig(
-    val hotbar: HotbarConfig,
+    val hotbar: Set<HotbarItemConfig>,
+    val visibilityCommand: VisibilityCommandConfig,
     val gamesGUI: GamesGUIConfig
 )
-
-@Suppress("PROVIDED_RUNTIME_TOO_LOW")
-@Serializable
-@SerialName("hotbar")
-data class HotbarConfig(
-    val navigatorItem: HotbarItemConfig
-) {
-    val items: Set<HotbarItemConfig> = setOf(navigatorItem)
-}
 
 @Suppress("PROVIDED_RUNTIME_TOO_LOW")
 @Serializable
@@ -28,4 +19,20 @@ data class HotbarItemConfig(
     val name: String,
     val description: String,
     val hotbarSlot: Int,
+    val commandOnClick: String
+)
+
+@Suppress("PROVIDED_RUNTIME_TOO_LOW")
+@Serializable
+data class VisibilityCommandConfig(
+    val item: VisibilityItemConfig,
+)
+
+@Suppress("PROVIDED_RUNTIME_TOO_LOW")
+@Serializable
+data class VisibilityItemConfig(
+    val slot: Int,
+    val materialOn: Material,
+    val materialOff: Material,
+    val enabled: Boolean
 )
