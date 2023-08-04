879181b26193d39f55f4fabf851c014d9795c1f7
Cizetux <cizetux@gmail.com>
2023-08-04 18:04:39 +0200

    chore: Add visibility command

---
+++ src/main/kotlin/com/github/rushyverse/hub/commannds/VisibilityCommand.kt
@@ -0,0 +1,68 @@
+package com.github.rushyverse.hub.commannds
+
+import com.github.rushyverse.api.koin.inject
+import com.github.rushyverse.api.player.ClientManager
+import com.github.rushyverse.hub.Hub
+import com.github.rushyverse.hub.Hub.Companion.BUNDLE_HUB
+import com.github.rushyverse.hub.client.ClientHub
+import com.github.shynixn.mccoroutine.bukkit.launch
+import dev.jorel.commandapi.kotlindsl.commandAPICommand
+import dev.jorel.commandapi.kotlindsl.playerExecutor
+import net.kyori.adventure.text.Component
+import net.kyori.adventure.text.format.NamedTextColor
+import org.bukkit.entity.Player
+
+class VisibilityCommand {
+
+    fun register(plugin: Hub) {
+        val clients: ClientManager by inject(plugin.id)
+        val commandConfig = plugin.config.visibilityCommand
+        val itemConfig = commandConfig.item
+
+        commandAPICommand("visibility") {
+            playerExecutor { player, _ ->
+
+                val world = player.world
+
+                plugin.launch {
+                    val client = clients.getClient(player) as ClientHub
+
+                    if (world != plugin.world) {
+                        val notAllowedMessage = Hub.translationsProvider.translate(
+                            "not.allowed.outside.hub",
+                            client.lang.locale,
+                            BUNDLE_HUB
+                        )
+                        client.send(Component.text(notAllowedMessage, NamedTextColor.RED))
+                        return@launch
+                    }
+
+                    val newVisibilityState = !client.canSeePlayers
+
+                    client.canSeePlayers(newVisibilityState, plugin)
+
+                    val message = Hub.translationsProvider.translate(
+                        "visibility.players.$newVisibilityState",
+                        client.lang.locale,
+                        BUNDLE_HUB
+                    )
+
+                    client.send(Component.text(message, NamedTextColor.GREEN))
+
+                    if (itemConfig.enabled) {
+                        val item = player.inventory.getItem(itemConfig.slot)
+                        player.inventory.setItem(itemConfig.slot,
+                            item?.apply {
+                                type = if (newVisibilityState)
+                                    itemConfig.materialOn
+                                else itemConfig.materialOff
+                            }
+                        )
+
+                    }
+                }
+
+            }
+        }
+    }
+}
