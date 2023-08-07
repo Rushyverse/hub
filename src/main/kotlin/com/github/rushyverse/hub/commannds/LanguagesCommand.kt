4576f770a1de58c1aa43be8efeec1af98fa53c59
Cizetux <cizetux@gmail.com>
2023-08-07 20:32:23 +0200

    chore: Update imports

--- src/main/kotlin/com/github/rushyverse/hub/commannds/LanguagesCommand.kt
+++ src/main/kotlin/com/github/rushyverse/hub/commannds/LanguagesCommand.kt
@@ -3,9 +3,11 @@ package com.github.rushyverse.hub.commannds
 import com.github.rushyverse.api.koin.inject
 import com.github.rushyverse.api.player.ClientManager
 import com.github.rushyverse.hub.Hub
+import com.github.rushyverse.hub.Hub.Companion.BUNDLE_HUB
 import com.github.shynixn.mccoroutine.bukkit.launch
 import dev.jorel.commandapi.kotlindsl.commandAPICommand
 import dev.jorel.commandapi.kotlindsl.playerExecutor
+import net.kyori.adventure.text.Component
 
 class LanguagesCommand {
 
