87e3ff9e5dcd3476ce62296d5ba5fde897d14ed5
Cizetux <cizetux@gmail.com>
2023-09-01 17:23:49 +0200

    chore(clean): Remove useless imports now

--- src/main/kotlin/com/github/rushyverse/hub/commannds/ProfileCommand.kt
+++ src/main/kotlin/com/github/rushyverse/hub/commannds/ProfileCommand.kt
@@ -4,12 +4,10 @@ import com.github.rushyverse.api.koin.inject
 import com.github.rushyverse.api.player.ClientManager
 import com.github.rushyverse.api.translation.getComponent
 import com.github.rushyverse.hub.Hub
-import com.github.rushyverse.hub.Hub.Companion.BUNDLE_HUB
 import com.github.rushyverse.hub.client.ClientHub
 import com.github.shynixn.mccoroutine.bukkit.launch
 import dev.jorel.commandapi.kotlindsl.commandAPICommand
 import dev.jorel.commandapi.kotlindsl.playerExecutor
-import net.kyori.adventure.text.Component
 import net.kyori.adventure.text.format.NamedTextColor
 
 class ProfileCommand {
