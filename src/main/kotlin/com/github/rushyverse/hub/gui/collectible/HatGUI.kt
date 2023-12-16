fcdbdc7a5d45a46d4a6101d96d829514a409f623
Cizetux <cizetux@gmail.com>
2023-12-16 13:05:05 +0100

    feat: Update HatGUI to use CosmeticDataInventory

--- src/main/kotlin/com/github/rushyverse/hub/gui/collectible/HatGUI.kt
+++ src/main/kotlin/com/github/rushyverse/hub/gui/collectible/HatGUI.kt
@@ -1,21 +1,30 @@
 package com.github.rushyverse.hub.gui.collectible
 
+import com.github.rushyverse.api.Plugin
 import com.github.rushyverse.hub.data.Cosmetic
 import org.bukkit.event.inventory.InventoryClickEvent
 import org.bukkit.inventory.ItemStack
 import org.bukkit.inventory.PlayerInventory
 
-class HatGUI : CosmeticGUI("gui.hats.title", 54) {
+class HatGUI(plugin: Plugin) : CosmeticGUI(plugin, "gui.hats.title") {
 
      override val cosmetics = listOf(
-        10 to Cosmetic.Hats.AstroHat,
-        11 to Cosmetic.Hats.RainbowHat,
-        12 to Cosmetic.Hats.LampHat,
-        13 to Cosmetic.Hats.SuperHat,
-        14 to Cosmetic.Hats.CrimsonHat
+        CosmeticDataInventory(10, Cosmetic.Hats.AstroHat) { },
+        CosmeticDataInventory(11, Cosmetic.Hats.RainbowHat) { },
+        CosmeticDataInventory(12, Cosmetic.Hats.LampHat) { },
+        CosmeticDataInventory(13, Cosmetic.Hats.SuperHat) { },
+        CosmeticDataInventory(14, Cosmetic.Hats.CrimsonHat) { }
     )
 
     override fun setItem(cosmeticItem: ItemStack, inventory: PlayerInventory, event: InventoryClickEvent) {
         inventory.helmet = cosmeticItem
+    }
+
+    override fun removeItem(inventory: PlayerInventory) {
+        inventory.helmet = null
+    }
+
+    override fun isSetItem(inventory: PlayerInventory, cosmeticItem: ItemStack): Boolean {
+        return inventory.helmet == cosmeticItem
     }
 }
